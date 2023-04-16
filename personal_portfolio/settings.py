922e6f6b71452d82c3cfd091f34a9b50295c3bc7
Caleb Crowder <jcrowder_caleb@yahoo.com>
2023-04-16 12:58:08 -0400

    Add Projects and Blog apps

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'projects',
+    'blog',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,10 @@ ROOT_URLCONF = 'personal_portfolio.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['personal_portfolio/templates/'],
+        'DIRS': [
+            'personal_portfolio/templates/',
+            'projects/static/',
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
